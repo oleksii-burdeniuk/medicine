624650c14d173dee7fcb977ca461f8a06fd74a6a
Oleksii Burdeniuk <oleksiiburdeniuk@MacBookPro.home>
2025-10-15 19:33:04 +0200

    analitics added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import './globals.css';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import Header from './components/Header';
 import Footer from './components/Footer';
-
+import { Analytics } from '@vercel/analytics/next';
 const geistSans = Geist({
   variable: '--font-geist-sans',
   subsets: ['latin'],
@@ -38,6 +38,7 @@ export default function RootLayout({
         {children}
 
         <SpeedInsights />
+        <Analytics />
         <Footer />
       </body>
     </html>
