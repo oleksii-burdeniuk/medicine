beae3b1224aaf9257a819779c25aaabd42cbb351
Oleksii Burdeniuk <alejandroburdenyk@gmail.com>
2026-03-24 15:16:51 +0100

    catch added

--- src/app/libs/analytics/gtag.ts
+++ src/app/libs/analytics/gtag.ts
@@ -5,15 +5,23 @@ export const GA_ID = 'G-PYHS0CDP9Z';
 // page view
 export const pageview = (url: string) => {
   if (typeof window !== 'undefined' && window.gtag) {
-    window.gtag('config', GA_ID, {
-      page_path: url,
-    });
+    try {
+      window.gtag('config', GA_ID, {
+        page_path: url,
+      });
+    } catch (error) {
+      console.error('Error sending pageview to Google Analytics:', error);
+    }
   }
 };
 
 // events
 export const event = (action: EventType, params?: Record<string, unknown>) => {
   if (typeof window !== 'undefined' && window.gtag) {
-    window.gtag('event', action, params);
+    try {
+      window.gtag('event', action, params);
+    } catch (error) {
+      console.error('Error sending event to Google Analytics:', error);
+    }
   }
 };
